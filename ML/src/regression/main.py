50c79e3ffc2eb69b9b61beb757d8f5d7b4dca463
maja <marirudolph@gmail.com>
2017-06-25 10:51:59 -0400

    neural network for bias prediction

--- ML/src/regression/main.py
+++ ML/src/regression/main.py
@@ -20,9 +20,10 @@ g_examples = glob.glob(data_folder +'train/g*')
 ### PARAMETERS
 
 mb = 200
-eta = 0.0001
+#eta = 0.0001
 lam = 1.0
 n_iter = 50000
+H0 = 10
 
 ### Embeddings
 #rho = np.random.randn(L, K)
@@ -36,13 +37,15 @@ L, K = emb.shape
 ### Parameters
 relevance = tf.nn.sigmoid(tf.Variable(np.random.randn(L).astype('float32')))
 print('NOT USING RELEVANCE')
-w = tf.Variable(0.001*np.random.randn(K,1).astype('float32'))
-#b = tf.Variable(0.0, tf.float32)
-b = 0.0
+
+trunc = np.sqrt(6)/np.sqrt(K + H0)
+w_1 = tf.Variable(np.random.uniform( -trunc, trunc, [K, H0]).astype('float32'))
+trunc = np.sqrt(6)/np.sqrt(H0)
+w_2 = tf.Variable(np.random.uniform( -trunc, trunc, [H0, 1]).astype('float32'))
 
 ### prior on w
 prior = Normal(loc = 0.0, scale = lam)
-log_prior = tf.reduce_sum(prior.log_prob(w) + prior.log_prob(b))
+log_prior = tf.reduce_sum(prior.log_prob(w_1)) + tf.reduce_sum(prior.log_prob(w_2))
 
 ### placeholders for data minibatches
 
@@ -59,14 +62,18 @@ def next_batch(file_list):
         features[i] = extract_features(np.load(file_list[idx]))
     return features
 
+def neural_network(x, w_1, w_2):
+    hidden = tf.tanh(tf.matmul(x, w_1))
+    return tf.matmul(hidden, w_2)
+
 ### LOGISTIC REGRESSION MODEL
 left_ex = tf.placeholder(tf.float32, shape = (mb, K))
 right_ex = tf.placeholder(tf.float32, shape = (mb, K))
 g_ex = tf.placeholder(tf.float32, shape = (mb, K))
 
-left_eta = b + tf.matmul(left_ex, w)
-right_eta = b + tf.matmul(right_ex, w)
-g_eta = b + tf.matmul(g_ex, w)
+left_eta = neural_network(left_ex, w_1, w_2)
+right_eta = neural_network(right_ex, w_1, w_2)
+g_eta = neural_network(g_ex, w_1, w_2)
 
 left_y = Bernoulli(logits = left_eta)
 right_y = Bernoulli(logits = right_eta)
@@ -79,7 +86,7 @@ neutral =  tf.reduce_mean(g_y.log_prob(0.5))
 loss = - (log_prior + 1000.0*(left_bias + right_bias + neutral))
 
 ### TRAINING
-optimizer = tf.train.AdamOptimizer(eta)
+optimizer = tf.train.AdamOptimizer()
 train = optimizer.minimize(loss)
 sess = tf.Session()
 with sess.as_default():
@@ -91,7 +98,8 @@ with tf.name_scope('objective'):
     tf.summary.scalar('neutral', neutral)
     tf.summary.scalar('right_bias', right_bias)
     tf.summary.scalar('left_bias', left_bias)
-variable_summaries('weights', w)
+variable_summaries('hidden_weights', w_1)
+variable_summaries('weights', w_2)
 variable_summaries('left_eta', left_eta)
 variable_summaries('right_eta', right_eta)
 variable_summaries('g_eta', g_eta)
@@ -117,6 +125,7 @@ for step in range(n_iter):
 with sess.as_default():
     dat = {'emb': emb,
            #'b': b.eval(),
-           'w': w.eval()}
+           'w_1': w_1.eval(),
+           'w_2': w_2.eval()}
 
 pickle.dump( dat, open( os.path.join(logdir, "model_params.pkl"), "a+" ) )
